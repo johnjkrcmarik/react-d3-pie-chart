68639f7def32a136e86645ad6d1c00bd14eb2c66
johnjkrcmarik <johnjkrcmarik@gmail.com>
2021-12-10 12:48:59 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import * as d3 from "d3";
+import PieClass from "./PieClass";
+import PieHooks from "./PieHooks";
+import PieSVG from "./PieSVG";
+import AnimatedPieHooks from "./AnimatedPieHooks";
+import AnimatedPieSVG from "./AnimatedPieSVG";
+
+import "./styles.css";
+
+function App() {
+  const generateData = (value, length = 5) =>
+    d3.range(length).map((item, index) => ({
+      date: index,
+      value: value === null || value === undefined ? Math.random() * 100 : value
+    }));
+
+  const [data, setData] = useState(generateData(0));
+  const changeData = () => {
+    setData(generateData());
+  };
+
+  useEffect(() => {
+    setData(generateData());
+  }, [!data]);
+
+  return (
+    <div className="App">
+      <div>
+        <button onClick={changeData}>Transform</button>
+      </div>
+      <div>
+        <span className="label">Animated Pie SVG (React Spring)</span>
+        <AnimatedPieSVG
+          data={data}
+          width={200}
+          height={200}
+          innerRadius={60}
+          outerRadius={100}
+        />
+      </div>
+      <div>
+        <span className="label">Animated Pie Hooks (D3 animations)</span>
+        <AnimatedPieHooks
+          data={data}
+          width={200}
+          height={200}
+          innerRadius={60}
+          outerRadius={100}
+        />
+      </div>
+      <div>
+        <span className="label">SVG Elements</span>
+        <PieSVG
+          data={data}
+          width={200}
+          height={200}
+          innerRadius={60}
+          outerRadius={100}
+        />
+      </div>
+      <div>
+        <span className="label">Hooks</span>
+        <PieHooks
+          data={data}
+          width={200}
+          height={200}
+          innerRadius={60}
+          outerRadius={100}
+        />
+      </div>
+      <div>
+        <span className="label">React Class</span>
+        <PieClass
+          data={data}
+          width={200}
+          height={200}
+          innerRadius={60}
+          outerRadius={100}
+        />
+      </div>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
